90855dabaaf1884748ae9b4895519bd3f4824cec
Andrew Gunnerson <annerson@us.ibm.com>
2016-07-15 11:14:12 -0400

    shell: Only set Android environment variables if the SDK/NDK is installed

--- files/shell/shell.d/40_env_android.sh
+++ files/shell/shell.d/40_env_android.sh
@@ -1,10 +1,15 @@
-export ANDROID_HOME=/opt/android-sdk
-export ANDROID_NDK=/opt/android-ndk-r11c
-export ANDROID_NDK_HOME="${ANDROID_NDK}"
-__build_tools_ver="$(find "${ANDROID_HOME}/build-tools" -mindepth 1 -maxdepth 1 | sort -V | tail -1)"
-__build_tools_ver="$(basename "${__build_tools_ver}")"
-path_push_back "${ANDROID_HOME}/tools"
-path_push_back "${ANDROID_HOME}/platform-tools"
-path_push_back "${ANDROID_HOME}/build-tools/${__build_tools_ver}"
-path_push_back "${ANDROID_NDK}"
-unset __build_tools_ver
+if [[ -d /opt/android-sdk ]]; then
+    export ANDROID_HOME=/opt/android-sdk
+    __build_tools_ver="$(find "${ANDROID_HOME}/build-tools" -mindepth 1 -maxdepth 1 | sort -V | tail -1)"
+    __build_tools_ver="$(basename "${__build_tools_ver}")"
+    path_push_back "${ANDROID_HOME}/tools"
+    path_push_back "${ANDROID_HOME}/platform-tools"
+    path_push_back "${ANDROID_HOME}/build-tools/${__build_tools_ver}"
+    unset __build_tools_ver
+fi
+
+if [[ -d /opt/android-ndk-r11c ]]; then
+    export ANDROID_NDK=/opt/android-ndk-r11c
+    export ANDROID_NDK_HOME="${ANDROID_NDK}"
+    path_push_back "${ANDROID_NDK}"
+fi

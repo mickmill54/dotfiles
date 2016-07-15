90d4f5563643afb993b6b59afc608a7908cb4eed
Andrew Gunnerson <annerson@us.ibm.com>
2016-07-15 15:30:44 -0400

    shell: Fix ccache environment with brew ccache on OSX

--- files/shell/shell.d/40_env_ccache.sh
+++ files/shell/shell.d/40_env_ccache.sh
@@ -1,6 +1,18 @@
-if is_distro Arch; then
-    path_push_front /usr/lib/ccache/bin
+paths=(
+    /usr/lib/ccache/bin
+    /usr/local/opt/ccache/libexec
+)
+
+for p in "${paths[@]}"; do
+    if [[ -d "${p}" ]]; then
+        path_push_front "${p}"
+    fi
+done
+
+unset p
+
+if [[ -d /stuff/ccache ]]; then
+    export CCACHE_DIR=/stuff/ccache
 fi
 
-export CCACHE_DIR=/stuff/ccache
 export CCACHE_NLEVELS=4

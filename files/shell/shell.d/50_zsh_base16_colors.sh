d14aad9f9320f4967451de061d4a6dfb7153c9dc
Andrew Gunnerson <annerson@us.ibm.com>
2016-07-21 17:55:35 -0400

    vim,bash: Switch to base16-tomorrow-night theme

--- files/shell/shell.d/50_zsh_base16_colors.sh
+++ files/shell/shell.d/50_zsh_base16_colors.sh
@@ -1,3 +1,3 @@
 if is_shell zsh; then
-    source "${__config_dotfiles_dir}/submodules/base16-shell/scripts/base16-default-dark.sh"
+    source "${__config_dotfiles_dir}/submodules/base16-shell/scripts/base16-tomorrow-night.sh"
 fi

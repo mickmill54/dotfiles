d2d6064c580e9559580371c169451627f739ca00
Andrew Gunnerson <annerson@us.ibm.com>
2016-10-24 09:44:49 -0400

    90_java_lookandfeel.sh: Only set _JAVA_OPTIONS on Linux

--- files/shell/shell.d/90_java_lookandfeel.sh
+++ files/shell/shell.d/90_java_lookandfeel.sh
@@ -1,3 +1,3 @@
-if ! is_distro Ubuntu; then
+if is_os Linux && ! is_distro Ubuntu; then
     export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel"
 fi

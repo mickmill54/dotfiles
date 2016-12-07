95efea3b4db68a800fd39f4dd813b6f97c0a64e5
Andrew Gunnerson <annerson@us.ibm.com>
2016-12-07 11:47:00 -0500

    40_env_kubernetes.sh: Default value handling for "<config>/" and "/<ns>"

--- files/shell/shell.d/40_env_kubernetes.sh
+++ files/shell/shell.d/40_env_kubernetes.sh
@@ -2,18 +2,28 @@ if which kubectl &>/dev/null; then
     alias k='kubectl'
 
     function kns() {
-        local config
-        local namespace
+        local config=
+        local namespace=
+        local configfile=
 
         if [[ "${1}" == */* ]]; then
             config=${1%%/*}
             namespace=${1#*/}
         else
-            config=${1}
+            namespace=${1}
+        fi
+
+        if [[ -z "${config}" ]]; then
+            configfile=config
+        else
+            configfile=config.${config}
+        fi
+
+        if [[ -z "${namespace}" ]]; then
             namespace=default
         fi
 
         shift
-        kubectl --kubeconfig="${HOME}/.kube/config.${config}" --namespace="${namespace}" "${@}"
+        kubectl --kubeconfig="${HOME}/.kube/${configfile}" --namespace="${namespace}" "${@}"
     }
 fi

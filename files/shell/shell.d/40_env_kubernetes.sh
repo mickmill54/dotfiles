badd4a27f629ed07fda6caa72b0e7f5af94c0b56
Andrew Gunnerson <annerson@us.ibm.com>
2017-03-13 12:13:54 -0400

    40_env_kubernetes.sh: Add usage text to kns()
    Signed-off-by: Andrew Gunnerson <annerson@us.ibm.com>

--- files/shell/shell.d/40_env_kubernetes.sh
+++ files/shell/shell.d/40_env_kubernetes.sh
@@ -1,10 +1,42 @@
 if which kubectl &>/dev/null; then
     alias k='kubectl'
 
+    function kns_usage() {
+        cat << EOF
+Usage: kns <pattern> [<kubectl arg> ...]
+
+The <pattern> argument must be provided and takes the following forms:
+
+* <config>
+  Use kubeconfig ~/.kube/config.<config> and default namespace
+* <config>/<namespace>
+  Use kubeconfig ~/.kube/config.<config> and namespace <namespace>
+* <config>/
+  Use kubeconfig ~/.kube/config.<config> and default namespace
+* /<namespace>
+  Use default kubeconfig and namespace <namespace>
+* /
+  Use default kubeconfig and default namespace
+
+Examples:
+* kns pd/jenkins ...
+  Use kubeconfig ~/.kube/config.pd and namespace 'jenkins'
+* kns pd ...
+  Use kubeconfig ~/.kube/config.pd and default namespace
+* kns /production
+  Use default kubeconfig and namespace 'production'
+EOF
+    }
+
     function kns() {
         local config=
         local namespace=
         local configfile=
+
+        if [[ "${1}" == -* ]]; then
+            kns_usage >&2
+            return 1
+        fi
 
         if [[ "${1}" == */* ]]; then
             config=${1%%/*}
